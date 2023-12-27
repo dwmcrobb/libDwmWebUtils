d90726955bbac62105279c9be8079f084ddc0d5e
Daniel McRobb <dmcrobb@one.ai>
2023-12-27 02:24:15 -0500

    Ubuntu 22.04 LTS repository has boost 1.74.  Work around it.

--- classes/include/DwmWebUtils.hh
+++ classes/include/DwmWebUtils.hh
@@ -43,7 +43,10 @@
 #define _DWMWEBUTILS_HH_
 
 #include <string>
-// #define BOOST_BEAST_USE_STD_STRING_VIEW  // deprecated
+#include <boost/version.hpp>
+#if (BOOST_VERSION < 108000)
+  #define BOOST_BEAST_USE_STD_STRING_VIEW // deprecated with newer boost
+#endif
 #include <boost/beast.hpp>
 #include <nlohmann/json.hpp>
 
